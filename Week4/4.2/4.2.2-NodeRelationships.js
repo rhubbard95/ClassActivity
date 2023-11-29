020482a5b584ba6fc7ae3bc7eb013d1a4c83da99
Rashad Hubbard <shad.hubbard90@gmail.com>
2023-11-28 20:32:19 -0600

    Noderelationships

---
+++ Week4/4.2/4.2.2-NodeRelationships.js
@@ -0,0 +1,13 @@
+// select the root node
+const root = document.querySelector("root");
+
+// select the last child of the root node
+document.querySelector("root").lastElementChild;
+
+// select all the children of the body element
+document.querySelectorAll("root").children;
+
+// select the next sibling of the h2 node
+document.querySelector("h2").nextElementSibling;
+// select the parent element of the h1 node
+document.querySelector("h1").parentElement;
