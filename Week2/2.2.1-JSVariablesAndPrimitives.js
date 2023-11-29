7490d619433007fcbaf7b66953f487646b7a7e7d
Rashad Hubbard <shad.hubbard90@gmail.com>
2023-11-28 19:55:30 -0600

    Noderelationships

---
+++ Week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,31 @@
+// create a constant variable (const)
+const name = "Rashad";
+// create a variable that can be reassigned (let)
+let language = "italian";
+// create three variables and assign them values of different data types (=)
+let age = 42;
+const whatif = true;
+const fancyPants = "cat's pajamas";
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof age);
+console.log(typeof fancyPants);
+// change the value referenced by a variable (dynamic typing)
+language = 7;
+// print the type of the variable you just changed (typeof)
+console.log(typeof language);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log(
+  "Hello, my name is " +
+    { name } +
+    ", and I am learning " +
+    { language } +
+    ". I have been practicing for only " +
+    { age } +
+    " weeks, so my status as a master is: " +
+    { whatif } +
+    "."
+);
---
+++ Week4/4.2/4.2.1-SelectingDOMElements.js
@@ -0,0 +1,14 @@
+// select all unordered list (ul) elements
+document.querySelectorAll("ul");
+
+document.getElementsByTagName("ul");
+
+// select all elements with the class "class-week"
+document.getElementsByClassName("class-week");
+
+// select all elements with the class "nav-bar"
+document.querySelectorAll("nav-bar");
+document.getElementsByClassName("nav-bar");
+
+// select the element with the id "dog-picture" and save it to a variable
+document.getElementById("dog-picture");
