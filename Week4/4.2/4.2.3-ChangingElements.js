367ab1e152d7c62ce042275bd43aff9aca3f4172
Rashad Hubbard <shad.hubbard90@gmail.com>
2023-11-28 21:24:47 -0600

    Changingelements

---
+++ Week4/4.2/4.2.3-ChangingElements.js
@@ -0,0 +1,17 @@
+// print the outer HTML of the navigation bar
+console.log(document.querySelector(".nav-bar").outerHTML);
+
+// print the inner HTML of the navigation bar
+console.log(document.querySelector(".nav-bar").innerHTML);
+
+// print the text content of the #class-schedule-list element
+console.log(document.querySelector("#class-schedule-list").textContext);
+
+// select the classList for the first class-week, then add the class "week-1"
+const firstClassWeek = document.querySelectorAll(.class-week);
+firstClassWeek.classList.add("week-1");
+// select the img element and add a src attribute
+const img = document.querySelector("img").add("src");
+
+// change the font color of the h1 element
+document.querySelector("h1").style.color = "red";
